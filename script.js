6fbb06f200918396511da709a1e2c4b09bbd0c91
typiicalxyah <90992709+typiicalxyah@users.noreply.github.com>
2021-09-18 22:49:18 -0400

    juntunu6

---
+++ script.js
@@ -0,0 +1,20 @@
+document.querySelector('h1').addEventListener('click' , ()=>{
+    document.body.style.background = 'red' ;
+    
+})
+
+
+document.querySelector('h2').addEventListener('click' , ()=>{
+    document.body.style.background = 'blue' ;
+
+})
+
+document.querySelector('h3').addEventListener('click' , ()=>{
+    document.body.style.background = 'green' ;
+    
+})
+
+document.querySelector('h4').addEventListener('click' , ()=>{
+    document.body.style.background = 'white' ;
+    
+})
